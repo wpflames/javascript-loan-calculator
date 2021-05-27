d4ead8fd95ce48bdda85cf0e469f1a1b2cc64402
Gabor Flamich <70385943+wpflames@users.noreply.github.com>
2021-05-27 12:10:34 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,83 @@
+// Listen for submit
+document.getElementById('loan-form').addEventListener('submit', function(e){
+    
+    // Hide results
+    document.getElementById('results').style.display = 'none';
+    
+    // Show loader
+    document.getElementById('loading').style.display = 'block';
+
+    // Set TimeOut
+    setTimeout(calculateResults, 2000);
+    
+    e.preventDefault();
+});
+
+// Calculate Results
+function calculateResults(e){
+    console.log('Calculating...');
+    //UI Variables
+    const amount = document.getElementById('amount');
+    const interest = document.getElementById('interest');
+    const years = document.getElementById('years');
+    const monthlyPayment = document.getElementById('monthly-payment');
+    const totalPayment = document.getElementById('total-payment');
+    const totalInterest = document.getElementById('total-interest');
+
+    // Calculations
+    const principal = parseFloat(amount.value);
+    const calculatedInterest = parseFloat(interest.value) / 100 /12;
+    const calculatedPayments = parseFloat(years.value) * 12;
+
+    // Compute Monthly Payments
+    const x = Math.pow(1 + calculatedInterest, calculatedPayments);
+    const monthly = (principal * x * calculatedInterest) / (x - 1);
+
+    if(isFinite(monthly)){
+        monthlyPayment.value = monthly.toFixed(2);
+        totalPayment.value = (monthly * calculatedPayments).toFixed(2);
+        totalInterest.value = ((monthly * calculatedPayments) - principal).toFixed(2);
+
+        // Show results
+        document.getElementById('results').style.display = 'block';
+
+        // Hide loader
+        document.getElementById('loading').style.display = 'none';
+    } else{
+        showError('Please check your numbers')
+    }
+
+}
+
+// Show Error
+function showError(error){
+    // Hide loaresultsder
+    document.getElementById('results').style.display = 'none';
+
+    // Hide loader
+    document.getElementById('loading').style.display = 'none';
+
+    // Create a div
+    const errorDiv = document.createElement('div');
+
+    // Get elements
+    const card = document.querySelector('.card');
+    const heading = document.querySelector('.heading');
+
+    // Add class
+    errorDiv.className = 'alert alert-danger';
+
+    //Create text node and append to div
+    errorDiv.appendChild(document.createTextNode(error));
+
+    // Insert error above heading
+    card.insertBefore(errorDiv, heading);
+
+    // Clear error after 3 seconds
+    setTimeout(clearError, 3000);
+}
+
+// Clear error
+function clearError(){
+    document.querySelector('.alert').remove();
+}
